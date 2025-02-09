7a3f61a51944f02c2572755f819ee161e7b616e9
m-gunel <gunayka264@gmail.com>
2025-02-10 00:52:33 +0300

    Update middleware_test.go

--- pkg/handler/middleware_test.go
+++ pkg/handler/middleware_test.go
@@ -5,8 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-	"github.com/zhashkevych/todo-app/pkg/service"
-	service_mocks "github.com/zhashkevych/todo-app/pkg/service/mocks"
 	"net/http/httptest"
 	"testing"
 )
